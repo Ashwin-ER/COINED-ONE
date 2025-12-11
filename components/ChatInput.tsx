ef102593956ba1fa8c2713bc3aee6bcbb1c5d2ea
Ashwin-AIEngr <ashwin.aiengineer@gmail.com>
2025-12-11 23:19:48 +0530

    Initial commit

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { Send, Sparkles } from 'lucide-react';
 
 interface ChatInputProps {
